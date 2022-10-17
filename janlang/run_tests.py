3e4a81f31fcf1699de7b30cbb6321a092d308654
evan <evfredericksen@gmail.com>
2022-10-16 21:49:44 -0400

    closure tests

--- janlang/run_tests.py
+++ janlang/run_tests.py
@@ -4,11 +4,14 @@ import argparse
 import interpreter
 import ast_json
 import lexer
+import errors
 import _parser as parser
 
 def main():
     arg_parser = argparse.ArgumentParser(description='Process some integers.')
     args = arg_parser.parse_args()
+    passed = []
+    failed = []
     for root, _, file_names in os.walk('../tests'):
         for file_name in file_names:
             path = os.path.join(root, file_name)
@@ -16,8 +19,14 @@ def main():
             tokenizer = lexer.RuleLexer.from_path(path)
             tokens = list(tokenizer.tokenize())
             program = parser.Parser(tokens).parse_root()
-            interpreter.Interpreter().execute(program)
-            print(path)
+            try:
+                interpreter.Interpreter().execute(program)
+            except errors.JanRuntimeException as e:
+                print(f'Exception in {path}')
+                failed.append((path, e))
+            else:
+                passed.append(path)
+    print(f'{len(passed)} tests passed, {len(failed)} tests had errors')
 
 if __name__ == '__main__':
     main()
